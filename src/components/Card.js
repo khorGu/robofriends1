98d95864daf77fef7004353f7bc77cbf982113b1
khorGu <bekaxorgu31@gmail.com>
2021-11-15 22:13:36 +0400

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+function Card({ name, email, id }) {
+  return (
+    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${name}?200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
